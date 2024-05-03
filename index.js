ee2548f6e7d44eb2b8e14f4fa5438ff29362e2c2
mstsurnalyakter <surnalyms@gmail.com>
2024-05-03 08:50:50 +0600

    project basic setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("I am from Home Route")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running at : http://localhost:${port}`);
+})
