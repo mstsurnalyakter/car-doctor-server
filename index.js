13b60386adf04c528401eb1b8a3d3e802bd72583
mstsurnalyakter <surnalyms@gmail.com>
2024-05-03 09:26:16 +0600

    read services api create

--- index.js
+++ index.js
@@ -26,10 +26,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+
+    app.get("/services", async (req,res)=>{
+            const result = await serviceCollection.find().toArray();
+            res.send(result);
+    })
+
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
